2a13ea6d093f633ba70432ad785afa8bc663c90a
Mithilesh27 <31652458+Mithilesh27@users.noreply.github.com>
2018-11-30 15:00:02 +0530

    Add files via upload

---
+++ new2.java
@@ -0,0 +1,10 @@
+public class new2{
+	public static void main(String[] args) {
+		new1 new1obj=new new1();
+		System.out.println(new1obj.toMilitary());
+		new1obj.setTime(13,27, 6);
+		System.out.println(new1obj.toMilitary());
+	}
+	
+	
+}
